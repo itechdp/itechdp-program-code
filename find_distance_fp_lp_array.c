f7295cc4fd954dad74decb52b6e069dd8cadd038
Dhrumil Patel <98255642+itechdp@users.noreply.github.com>
2022-01-31 15:21:45 +0530

    Find Distance From First Point To Last Point Array

---
+++ find_distance_fp_lp_array.c
@@ -0,0 +1,25 @@
+// 10 different points are input through the keyboard find out the distance between one point the last point
+
+#include <stdio.h>
+
+int main()
+{
+    int dis[10];
+    int i, distance = 0;
+    printf("Enter the distance of ten points");
+
+    for (i = 0; i < 10; i++)
+    {
+        printf("\nPoint %d : ", i);
+        scanf("%d", &dis[i]);
+    }
+
+    for (i = 0; i < 10; i++)
+    {
+        distance = distance + dis[i];
+    }
+
+    printf("\nThe distance between first point to last point is:%d", distance);
+
+    return 0;
+}
